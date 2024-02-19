cd6733be4d09801363e91d1249706d99df844069
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-19 13:36:14 +0300

    feat: add content to the footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,17 +12,19 @@ export default function Footer() {
     <footer id="footer">
       <hr className="w-11/12 mx-auto" />
 
-      <section className="container py-20 flex justify-center items-center">
-        <div>
-          <Image
-            src="/assets/email.svg"
-            width={200}
-            height={200}
-            alt="Email
-          "
-          />{" "}
+      <section className="container py-20 flex flex-col md:flex-row justify-center items-center md:gap-24 gap-12">
+        <div className="">
+          <Image src="/assets/email.svg" width={200} height={200} alt="Email" />{" "}
         </div>
-        <div></div>
+        <div className="flex flex-col w-[20rem] gap-4 max-md:justify-center max-md:items-center">
+          <h1 className="text-xl font-bold">تواصل معنا!</h1>
+          <div>
+            <p className="text-md max-md:text-center text-muted-foreground md:w-10/12 mx-auto lg:mx-0">
+              نحرص على تنفيذ مقترحاتكم وملاحظاتكم وحل المشاكل التي تواجهكم في
+              الموقع، راسلونا عبر البريد الإلكتروني: mostathmerplus@gmail.com
+            </p>
+          </div>
+        </div>
       </section>
 
       <section className="container pb-14 text-center">
