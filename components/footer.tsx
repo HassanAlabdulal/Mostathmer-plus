863777f7ad8caadfe604c0dc8a3af59b9a1d6ec2
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-19 21:00:23 +0300

    enhance: adjust text sizes in all screens

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import {
-  GitHubLogoIcon,
-  LinkedInLogoIcon,
-  TwitterLogoIcon,
-} from "@radix-ui/react-icons";
+// import {
+//   GitHubLogoIcon,
+//   LinkedInLogoIcon,
+//   TwitterLogoIcon,
+// } from "@radix-ui/react-icons";
 import Image from "next/image";
 
 export default function Footer() {
@@ -13,7 +13,7 @@ export default function Footer() {
       <hr className="w-11/12 mx-auto" />
 
       <section className=" py-20 flex flex-col md:flex-row justify-center items-center md:gap-16 gap-12">
-        <div className="max-md:w-[170px] max-md:h-[170px]">
+        <div className="max-md:w-[150px] max-md:h-[150px]">
           <Image src="/assets/email.svg" width={200} height={200} alt="Email" />
         </div>
         <div className="flex flex-col w-[20rem] gap-4 justify-center max-md:items-center">
@@ -28,19 +28,13 @@ export default function Footer() {
       </section>
 
       <section className=" pb-8 flex justify-center items-center gap-4">
-        <div className="h-8 w-8">
-          <GitHubLogoIcon />
-        </div>
-        <div className="h-8 w-8">
-          <LinkedInLogoIcon />
-        </div>
-        <div className="h-8 w-8">
-          <TwitterLogoIcon />
-        </div>
+        <div className="h-8 w-8">{/* <GitHubLogoIcon /> */}</div>
+        <div className="h-8 w-8">{/* <LinkedInLogoIcon /> */}</div>
+        <div className="h-8 w-8">{/* <TwitterLogoIcon /> */}</div>
       </section>
 
       <section className="container pb-14 text-center">
-        <h3 className="flex gap-1 justify-center items-center">
+        <h3 className="flex gap-1 justify-center items-center md:text-base text-sm text-muted-foreground ">
           جميع الحقوق محفوظة لفريق مُستثمر بلس &copy; ٢٠٢٤
         </h3>
       </section>
