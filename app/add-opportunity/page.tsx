703c12a550e756c7faddb0b99346e236aa49cad7
Abdullah Ali <abdullaham1422@gmail.com>
2024-02-20 21:08:35 +0300

    enhance: the time and cost field

--- app/add-opportunity/page.tsx
+++ app/add-opportunity/page.tsx
@@ -15,8 +15,8 @@ type FormValues = {
   investmentType: string;
   investmentTimeUnit: string; 
   investmentTimeValue: number;
+  projectValueUnit: string;
   projectValue: number;
-  amountRaised: number;
   investmentLocation: string;
   contactDetails: string;
   attachments: FileList;
@@ -35,11 +35,16 @@ export default function AddOpportunity() {
     register('investmentType', { required: 'هذا الحقل مطلوب' });
   }, [register]);
 
+  const cities = [
+    "الرياض", "جدة", "مكة المكرمة", "المدينة المنورة", "الدمام", "الخبر",
+    "الطائف", "تبوك", "القطيف", "خميس مشيط", "الأحساء", "حائل"
+  ];
+
   // Function to handle form submission
   const onSubmit = (data: FormValues) => console.log(data);
 
   return (
-    <div className="flex flex-col justify-center min-h-screen py-12 bg-gray-50 sm:px-6 lg:px-8">
+    <div className="flex flex-col justify-center min-h-screen py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-2xl">
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-6 p-8 rounded shadow">
           {/* Page title */}
@@ -54,7 +59,7 @@ export default function AddOpportunity() {
 
           <div>
             <label htmlFor="detailedDescription">وصف تفصيلي</label>
-            <Textarea id="detailedDescription" {...register('detailedDescription', { required: 'هذا الحقل مطلوب' })} />
+            <Textarea className="resize-none" id="detailedDescription" {...register('detailedDescription', { required: 'هذا الحقل مطلوب' })} />
             {errors.detailedDescription && <p>{errors.detailedDescription.message}</p>}
           </div>
 
@@ -75,47 +80,83 @@ export default function AddOpportunity() {
             {errors.investmentType && <p>{errors.investmentType.message}</p>}
           </div>
 
-          {/* Investment Duration Selection */}
-          <div className="flex justify-between">
+          <div className="flex items-end">
             {/* Investment Time Value Section */}
-            <div className="flex-1 mr-2">
+            <div className="flex-1">
               <label htmlFor="investmentTimeValue">المدة الزمنية</label>
-              <select id="investmentTimeValue" {...register('investmentTimeValue', { required: 'هذا الحقل مطلوب' })} className="w-full">
+              <select
+                id="investmentTimeValue"
+                {...register('investmentTimeValue', { required: 'هذا الحقل مطلوب' })}
+                className="form-select form-select-lg mb-3 appearance-none
+                           block w-full px-4 py-2 text-xl font-normal 
+                           bg-clip-padding bg-no-repeat border border-solid border-gray-300
+                           rounded transition ease-in-out m-0
+                           focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+              >
                 {Array.from({ length: 12 }, (_, index) => (
-                  <option key={index} value={index + 1}>
-                    {index + 1}
-                  </option>
+                  <option key={index} value={index + 1}>{index + 1}</option>
                 ))}
               </select>
               {errors.investmentTimeValue && <p>{errors.investmentTimeValue.message}</p>}
             </div>
 
             {/* Investment Time Unit Section */}
-            <div className="flex-1 ml-2">
-              <select id="investmentTimeUnit" {...register('investmentTimeUnit', { required: 'هذا الحقل مطلوب' })} className="w-full">
-              <option value="months">شهور</option>
-              <option value="years">سنوات</option>
+            <div className="flex-1 ml-3">
+              <select
+                id="investmentTimeUnit"
+                {...register('investmentTimeUnit', { required: 'هذا الحقل مطلوب' })}
+                className="form-select form-select-lg mb-3 mr-1
+                           block px-4 py-2 text-xl font-normal 
+                            bg-clip-padding bg-no-repeat border border-solid border-gray-300
+                           rounded transition ease-in-out m-0
+                           focus:bg-white focus:border-blue-600 focus:outline-none"
+              >
+                <option value="months">شهور</option>
+                <option value="years">سنوات</option>
               </select>
               {errors.investmentTimeUnit && <p>{errors.investmentTimeUnit.message}</p>}
             </div>
           </div>
 
-          <div>
-            <label htmlFor="projectValue">القيمة المقدرة للمشروع</label>
-            <Input id="projectValue" type="number" {...register('projectValue', { required: 'هذا الحقل مطلوب' })} />
-            {errors.projectValue && <p>{errors.projectValue.message}</p>}
-          </div>
+            {/* Project Value and Unit in the same row with the same height */}
+            <div className="flex items-center">
 
-          <div>
-            <label htmlFor="amountRaised">المبلغ الذي تم جمعه حتى الآن</label>
-            <Input id="amountRaised" type="number" {...register('amountRaised')} />
-            {errors.amountRaised && <p>{errors.amountRaised.message}</p>}
-          </div>
+            {/* Project Value Input Section */}
+            <div className="flex-1">
+              <label htmlFor="projectValue">قيمة المشروع</label>
+              <input 
+                id="projectValue" 
+                type="text" 
+                {...register('projectValue', { required: 'هذا الحقل مطلوب' })} 
+                className="w-full h-10 border border-gray-300 rounded-md shadow-sm"
+              />
+              {errors.projectValue && <p>{errors.projectValue.message}</p>}
+            </div>
+         
+            {/* Project Value Unit Section */}
+            <div className="flex-1">
+              <select 
+                id="projectValueUnit" 
+                {...register('projectValueUnit', { required: 'هذا الحقل مطلوب' })} 
+                className="mr-1 mt-6 h-10 border border-gray-300 rounded-md shadow-sm"
+              >
+                <option value="thousand">ألف</option>
+                <option value="million">مليون</option>
+              </select>
+              {errors.projectValueUnit && <p>{errors.projectValueUnit.message}</p>}
+            </div>
+
+            </div>
 
           {/* Investment Location Section */}
           <div>
             <label htmlFor="investmentLocation">موقع الاستثمار</label>
-            <Input id="investmentLocation" {...register('investmentLocation', { required: 'هذا الحقل مطلوب' })} />
+            <select id="investmentLocation" {...register('investmentLocation', { required: 'هذا الحقل مطلوب' })}>
+              <option value="">اختر مدينة...</option>
+              {cities.map((city, index) => (
+                <option key={index} value={city}>{city}</option>
+              ))}
+            </select>
             {errors.investmentLocation && <p>{errors.investmentLocation.message}</p>}
           </div>
 
