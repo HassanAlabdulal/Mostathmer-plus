014203ed9b204ec5edc1eea70a694f20f0f359f7
HassanAlabdulal <hhhalabdulaal@gmail.com>
2024-02-20 14:44:23 +0300

    enhance: change text sizes

--- components/sections/homepage.tsx
+++ components/sections/homepage.tsx
@@ -5,7 +5,7 @@ export default function Homepage() {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center space-y-6 flex justify-center items-center flex-col">
-        <main className="text-4xl md:text-7xl font-bold md:leading-tight leading-normal tracking-tight  ">
+        <main className="text-2xl md:text-5xl font-bold md:leading-tight leading-normal tracking-tight  ">
           <h1 className="inline">
             <span className="inline bg-gradient-to-r from-[#3cb468]  to-[#30a059] text-transparent bg-clip-text">
               مُسثمر بلس:{" "}
